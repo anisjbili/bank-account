421fb331c2540bc2e90e3fe230e92fd3b62c568b
AnisJb <anis.jbilii@gmail.com>
2022-11-23 12:24:10 +0100

    Fix the test of deposit of negative amount

--- src/main/java/com/kata/bankaccount/business/Amount.java
+++ src/main/java/com/kata/bankaccount/business/Amount.java
@@ -1,13 +1,17 @@
 package com.kata.bankaccount.business;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public final class Amount {
 
     private final BigDecimal value;
 
     public Amount(BigDecimal value) {
-       this.value = value;
+        Objects.requireNonNull(value);
+        if(value.signum()==-1)
+            throw new IllegalArgumentException("Amount must not be negative");
+        this.value = value;
     }
 
     public BigDecimal getValue() {
