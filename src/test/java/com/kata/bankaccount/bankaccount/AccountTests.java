924da4e22d58ef650ddc05a59d362b173c5d8078
AnisJb <anis.jbilii@gmail.com>
2022-11-23 11:39:12 +0100

    Initial commit

---
+++ src/test/java/com/kata/bankaccount/bankaccount/AccountTests.java
@@ -0,0 +1,11 @@
+package com.kata.bankaccount.bankaccount;
+
+import org.junit.jupiter.api.Test;
+
+public class AccountTests {
+
+    @Test
+    void should_new_account_have_balance_of_10_when_deposit_an_amount_of_10() {
+
+    }
+}
